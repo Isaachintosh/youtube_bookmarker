bc7b3477a5e1a8410f811651e6eed1b93f228db8
isaachintosh <isaac.tuning@gmail.com>
2022-07-27 20:25:28 -0300

    checking if the video has bookmarks

--- popup.js
+++ popup.js
@@ -1,3 +1,5 @@
+import { getActiveTabURL } from "./utils.js"
+
 const addNewBookmark = () => {};
 
 const viewBookmarks = () => {};
@@ -8,4 +10,24 @@ const onDelete = () => {};
 
 const setBookmarkAttributes = () => {};
 
-document.addEventListener("DOMContentLoaded", () => {});
+document.addEventListener("DOMContentLoaded", async () => {
+    const activeTab = await getActiveTabURL();
+    const queryParameters = activeTab.url.split("?")[1];
+    const urlParameters = new URLSearchParams(queryParameters);
+
+    const currentVideo = urlParameters.get("v");
+
+    if (activeTab.url.includes("youtube.com/watch") && currentVideo) {
+        chrome.storage.sync.get([currentVideo], (data) => {
+            const currentVideoBookmarks = data[currentVideo] ? JSON.parse(data[currentVideo]): [];
+
+
+        });
+    } else {
+        const container = document.getElementsByClassName("container")[0];
+        
+        container.innerHTML = '<div class="title">This is not a youtube video page.</div>';
+
+        
+    }
+});
